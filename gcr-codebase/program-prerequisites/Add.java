c95c57fb69ed80e5c5927a4f638b7c13eb144ef9
shifak2005 <sheikhshifa748@gmail.com>
2025-12-16 16:35:10 +0530

    [Shifa Khan] : added Addition program

---
+++ gcr-codebase/program-prerequisites/Add.java
@@ -0,0 +1,17 @@
+class Add{
+
+public static int addTwoNumbers(int a, int b)
+{
+     int c=a+b;
+return c;
+}
+public static void main(String args[])
+{
+   int a=2;
+   int b=4;
+   int ans=addTwoNumbers(a,b);
+  System.out.println(ans);
+
+}
+
+}
