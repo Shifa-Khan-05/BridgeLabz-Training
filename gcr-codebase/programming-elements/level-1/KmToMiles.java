25d91221a2f1ea6b32274cfeded671832f91274e
shifak2005 <sheikhshifa748@gmail.com>
2025-12-17 13:34:08 +0530

    [Shifa Khan] : added Km to Mile conversion program

---
+++ gcr-codebase/programming-elements/level-1/KmToMiles.java
@@ -0,0 +1,16 @@
+public class KmToMiles {
+
+public static void kmToMiles()
+{
+double km=10.8;
+double miles=km*1.6;
+System.out.println("The distance  " + km + "km in miles is " + miles)
+
+}
+public static void main(String args[])
+{
+ kmToMiles();
+
+}
+
+}
