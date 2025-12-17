707a7007c42622299356487ef729a901912cab01
shifak2005 <sheikhshifa748@gmail.com>
2025-12-17 13:21:15 +0530

    [Shifa Khan] : added Sam Marks program

---
+++ gcr-codebase/programming-elements/level-1/SamMarks.java
@@ -0,0 +1,17 @@
+public class SamMarks
+{
+public static int average()
+{    
+  int physics=95,chemistry=96,maths=94;
+  int avg=(physics+maths+chemistry)/3;
+  return avg;
+  
+}
+
+	public static void main(String args[])
+	{
+	
+	  System.out.println(average()); 
+	}
+
+}
