0c07833b390d348cb9ae319f6c1a4f90ef0ab841
shifak2005 <sheikhshifa748@gmail.com>
2025-12-27 22:45:57 +0530

    [Shifa khan] : add program for Bank account management class BankAccount

---
+++ java-oops-practice/gcr-codebase/javaOopsConcept/src/com/constructors/level1/BankAccount.java
@@ -0,0 +1,31 @@
+package com.constructors.level1;
+
+// Creating BankAccount class
+public class BankAccount {
+
+    // Public variable (accessible everywhere)
+    public int accountNumber;
+
+    // Protected variable (accessible in subclass)
+    protected String accountHolder;
+
+    // Private variable (data hiding)
+    private double balance;
+
+    // Constructor to initialize account details
+    public BankAccount(int accountNumber, String accountHolder, double balance) {
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+    }
+
+    // Public method to get balance
+    public double getBalance() {
+        return balance;
+    }
+
+    // Public method to modify balance
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
