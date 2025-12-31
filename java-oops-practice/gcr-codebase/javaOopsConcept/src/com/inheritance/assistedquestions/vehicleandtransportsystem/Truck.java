bfeb79b5306bcdf6ec4cd4f8693c486ca9f53c4b
shifak2005 <sheikhshifa748@gmail.com>
2025-12-31 23:12:00 +0530

    [Shifa Khan] : added a program with vehicle hierarchy where Vehicle is the superclass, and Car, Truck, and Motorcycle are subclasses with unique attributes.

---
+++ java-oops-practice/gcr-codebase/javaOopsConcept/src/com/inheritance/assistedquestions/vehicleandtransportsystem/Truck.java
@@ -0,0 +1,18 @@
+package com.inheritance.assistedquestions.vehicleandtransportsystem;
+
+class Truck extends Vehicle {
+
+    int loadCapacity; // in tons
+
+    Truck(int maxSpeed, String fuelType, int loadCapacity) {
+        super(maxSpeed, fuelType);
+        this.loadCapacity = loadCapacity;
+    }
+
+    @Override
+    void displayInfo() {
+        super.displayInfo();
+        System.out.println("Vehicle Type : Truck");
+        System.out.println("Load Capacity: " + loadCapacity + " tons");
+    }
+}
