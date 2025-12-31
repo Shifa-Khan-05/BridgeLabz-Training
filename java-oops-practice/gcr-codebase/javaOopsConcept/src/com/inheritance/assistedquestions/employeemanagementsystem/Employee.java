beddc0ba0bd68d12459afb3861c7be73dc064cd5
shifak2005 <sheikhshifa748@gmail.com>
2025-12-31 23:11:11 +0530

    [Shifa Khan] : added a program with Employee hierarchy for different employee types such as Manager, Developer, and Intern.

---
+++ java-oops-practice/gcr-codebase/javaOopsConcept/src/com/inheritance/assistedquestions/employeemanagementsystem/Employee.java
@@ -0,0 +1,23 @@
+package com.inheritance.assistedquestions.employeemanagementsystem;
+
+//Base Class
+class Employee {
+
+ String name;
+ int id;
+ double salary;
+
+ // Constructor
+ Employee(String name, int id, double salary) {
+     this.name = name;
+     this.id = id;
+     this.salary = salary;
+ }
+
+ // Method to display employee details
+ void displayDetails() {
+     System.out.println("Name   : " + name);
+     System.out.println("ID     : " + id);
+     System.out.println("Salary : ₹" + salary);
+ }
+}
