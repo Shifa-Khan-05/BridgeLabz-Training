cef8d82148f4642c1079348713a093fb7cac04eb
shifak2005 <sheikhshifa748@gmail.com>
2025-12-31 23:08:50 +0530

    [Shifa Khan] : added a program with hierarchy where Animal is the superclass, and Dog, Cat, and Bird are subclasses'

---
+++ java-oops-practice/gcr-codebase/javaOopsConcept/src/com/inheritance/assistedquestions/animalhierarchy/Cat.java
@@ -0,0 +1,14 @@
+package com.inheritance.assistedquestions.animalhierarchy;
+
+public class Cat extends Animal {
+
+	Cat(String name, int age) {
+        super(name, age);
+    }
+	 @Override
+     void makeSound()
+     {
+    	 System.out.println(" Cat make sound - meow!!!! ");
+     }
+	
+}
