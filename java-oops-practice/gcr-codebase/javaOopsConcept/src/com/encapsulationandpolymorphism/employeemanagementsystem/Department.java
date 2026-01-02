c22c70728b35a9f1c3b1c9ed805d2434d2c6d71e
shifak2005 <sheikhshifa748@gmail.com>
2026-01-02 15:25:59 +0530

    [Shifa Khan] : added a program that  Build an employee management system with the following requirements:

---
+++ java-oops-practice/gcr-codebase/javaOopsConcept/src/com/encapsulationandpolymorphism/employeemanagementsystem/Department.java
@@ -0,0 +1,7 @@
+package com.encapsulationandpolymorphism.employeemanagementsystem;
+
+interface Department {
+    void assignDepartment(String departmentName);
+    String getDepartmentDetails();
+}
+
