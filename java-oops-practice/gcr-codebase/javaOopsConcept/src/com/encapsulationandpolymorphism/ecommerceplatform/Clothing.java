2bc26fbdb198b84b4457fca2b81ce5b92428f04b
shifak2005 <sheikhshifa748@gmail.com>
2026-01-02 15:32:14 +0530

    [Shifa Khan] :  Develop a simplified e-commerce platform

---
+++ java-oops-practice/gcr-codebase/javaOopsConcept/src/com/encapsulationandpolymorphism/ecommerceplatform/Clothing.java
@@ -0,0 +1,13 @@
+package com.encapsulationandpolymorphism.ecommerceplatform;
+
+public class Clothing extends Product {
+
+    public Clothing(int id, String name, double price) {
+        super(id, name, price);
+    }
+
+    @Override
+    double calculateDiscount(double price) {
+        return price > 5000 ? price * 0.20 : price * 0.10;
+    }
+}
