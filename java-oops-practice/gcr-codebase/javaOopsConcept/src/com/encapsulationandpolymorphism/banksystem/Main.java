c898d221fec9a72fabf50ea2873a841679507765
shifak2005 <sheikhshifa748@gmail.com>
2026-01-02 15:21:27 +0530

    [Shifa Khan] : added a program with n: Create a banking system with different account types:

---
+++ java-oops-practice/gcr-codebase/javaOopsConcept/src/com/encapsulationandpolymorphism/banksystem/Main.java
@@ -0,0 +1,24 @@
+package com.encapsulationandpolymorphism.banksystem;
+
+import java.util.*;
+public class Main {
+	
+
+	    public static void main(String[] args) {
+
+	        List<BankAccount> accounts = new ArrayList<>();
+
+	        accounts.add(new SavingsAccount("SB101", "Amit", 50000));
+	        accounts.add(new CurrentAccount("CA202", "Riya", 80000));
+
+	        for (BankAccount acc : accounts) {
+	            System.out.println("Account Holder: " + acc.getHolderName());
+	            System.out.println("Balance: " + acc.getBalance());
+	            System.out.println("Interest Earned: " + acc.calculateInterest());
+
+	            acc.applyForLoan(30000);
+	           
+	        }
+	}
+
+}
