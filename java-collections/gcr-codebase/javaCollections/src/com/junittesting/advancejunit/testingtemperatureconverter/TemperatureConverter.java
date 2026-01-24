75d2ccf58ea2415ff5babf6271ea5377e1dfdd23
shifa-khan-05 <sheikhshifa748@gmail.com>
2026-01-24 13:01:45 +0530

    [Shifa Khan] : added a program  Testing Temperature Converter

---
+++ java-collections/gcr-codebase/javaCollections/src/com/junittesting/advancejunit/testingtemperatureconverter/TemperatureConverter.java
@@ -0,0 +1,15 @@
+package com.junittesting.advancejunit.testingtemperatureconverter;
+
+//Converts temperature between Celsius and Fahrenheit
+public class TemperatureConverter {
+
+ // Converts Celsius to Fahrenheit
+ public double celsiusToFahrenheit(double celsius) {
+     return (celsius * 9 / 5) + 32;
+ }
+
+ // Converts Fahrenheit to Celsius
+ public double fahrenheitToCelsius(double fahrenheit) {
+     return (fahrenheit - 32) * 5 / 9;
+ }
+}
