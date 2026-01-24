7032626768cbbc4f33a797197ac6c2fe2ae3558f
shifa-khan-05 <sheikhshifa748@gmail.com>
2026-01-24 12:52:11 +0530

    [Shifa Khan] : added a program Testing Parameterized Tests

---
+++ java-collections/gcr-codebase/javaCollections/src/com/junittesting/testingparameterizedtests/NumberUtils.java
@@ -0,0 +1,9 @@
+package com.junittesting.testingparameterizedtests;
+
+//Utility class for number operations
+public class NumberUtils {
+
+ public boolean isEven(int number) {
+     return number % 2 == 0;
+ }
+}
