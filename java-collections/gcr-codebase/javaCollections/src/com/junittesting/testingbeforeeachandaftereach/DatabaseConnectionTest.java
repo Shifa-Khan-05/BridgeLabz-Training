0dfac97e48b0aa4915d946e2cc10f75fbab51e55
shifa-khan-05 <sheikhshifa748@gmail.com>
2026-01-24 12:51:41 +0530

    [Shifa Khan] : added a program Testing @BeforeEach and @AfterEach Annotations

---
+++ java-collections/gcr-codebase/javaCollections/src/com/junittesting/testingbeforeeachandaftereach/DatabaseConnectionTest.java
@@ -0,0 +1,30 @@
+package com.junittesting.testingbeforeeachandaftereach;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Test;
+
+// JUnit 4 lifecycle methods example
+public class DatabaseConnectionTest {
+
+    DatabaseConnection db;
+
+    // Runs before every test
+    @Before
+    public void setUp() {
+        db = new DatabaseConnection();
+        db.connect();
+    }
+
+    // Runs after every test
+    @After
+    public void tearDown() {
+        db.disconnect();
+    }
+
+    @Test
+    public void testConnectionEstablished() {
+        assertTrue(db.isConnected());
+    }
+}
