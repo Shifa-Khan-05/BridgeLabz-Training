831ff4526bfc4b031b120f955c77cb4f5e4ccd33
shifa-khan-05 <sheikhshifa748@gmail.com>
2026-01-24 23:11:17 +0530

    [Shifa Khan] : added a program DealTracker – E-Commerce Discount Validator

---
+++ java-collections/scenarioBasedQuestions/src/com/dealtracker/Deal.java
@@ -0,0 +1,13 @@
+package com.dealtracker;
+
+
+
+public class Deal<T> {
+    String code;
+    int discount;
+
+    public Deal(String c, int d) {
+        code = c;
+        discount = d;
+    }
+}
