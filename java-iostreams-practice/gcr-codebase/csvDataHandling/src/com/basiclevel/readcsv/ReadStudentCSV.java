72cf5fd2b01766f30e5b4136c2163822778b34ab
shifa-khan-05 <sheikhshifa748@gmail.com>
2026-01-25 23:57:29 +0530

    [Shifa Khan] : added a program to read a csv file

---
+++ java-iostreams-practice/gcr-codebase/csvDataHandling/src/com/basiclevel/readcsv/ReadStudentCSV.java
@@ -0,0 +1,26 @@
+package com.basiclevel.readcsv;
+
+import java.io.*;
+public class ReadStudentCSV {
+
+    public static void main(String[] args) {
+        String filePath = "C:\\Users\\HP\\Desktop\\Java Programming Workspace\\java-iostreams-practice\\gcr-codebase\\csvDataHandling\\src\\com\\basiclevel\\readcsv\\students.csv";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            br.readLine(); // Skip header
+
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(",");
+
+                System.out.println("ID: " + data[0] +
+                        ", Name: " + data[1] +
+                        ", Age: " + data[2] +
+                        ", Marks: " + data[3]);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
