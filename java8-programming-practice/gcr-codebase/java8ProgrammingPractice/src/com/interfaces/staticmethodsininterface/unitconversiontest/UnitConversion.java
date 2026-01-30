af3d0bac2bddd0faace5844ea73b69ca01395915
shifa-khan-05 <sheikhshifa748@gmail.com>
2026-01-30 21:18:21 +0530

    [Shifa Khan] : added a program Unit Conversion Tool

---
+++ java8-programming-practice/gcr-codebase/java8ProgrammingPractice/src/com/interfaces/staticmethodsininterface/unitconversiontest/UnitConversion.java
@@ -0,0 +1,11 @@
+package com.interfaces.staticmethodsininterface.unitconversiontest;
+
+public interface UnitConversion {
+	static double kmToMiles(double km) {
+        return km * 0.621371;
+    }
+
+    static double kgToPounds(double kg) {
+        return kg * 2.20462;
+    }
+}
