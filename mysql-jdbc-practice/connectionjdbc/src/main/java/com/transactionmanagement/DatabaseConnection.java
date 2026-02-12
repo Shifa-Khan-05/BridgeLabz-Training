210472b72b19cb6848f642a44d6323ee42a48be7
shifa-khan-05 <sheikhshifa748@gmail.com>
2026-02-12 22:34:23 +0530

    Added transaction management implementation using JDBC

---
+++ mysql-jdbc-practice/connectionjdbc/src/main/java/com/transactionmanagement/DatabaseConnection.java
@@ -0,0 +1,24 @@
+package com.transactionmanagement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class DatabaseConnection {
+	
+
+
+	    private static final String URL = "jdbc:mysql://localhost:3306/jdbcconnection";
+	    private static final String USER = "root";
+	    private static final String PASSWORD = "05052005";
+
+	    // Get database connection
+	    public static Connection getConnection() throws SQLException {
+	        try {
+	            Class.forName("com.mysql.cj.jdbc.Driver");
+	            return DriverManager.getConnection(URL, USER, PASSWORD);
+	        } catch (ClassNotFoundException e) {
+	            throw new SQLException("MySQL Driver not found", e);
+	        }
+	    }
+	
+
+}
